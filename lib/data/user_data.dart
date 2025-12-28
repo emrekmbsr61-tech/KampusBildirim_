aff0d248d345e5fdeece6b6cf9c6a51ebd12d036
Yunus Emre POLAT <yunusemre21625@gmail.com>
2025-12-29 01:05:47 +0300

    Sisteme girecek örnek kullanıcaları tanımladım

---
+++ lib/data/user_data.dart
@@ -0,0 +1,9 @@
+// Burası uygulamada kullanacağımız örnek kullanıcı veritabanı
+
+final Map<String, String> kayitliKullanicilar = {
+  // 'E-posta' : 'Şifre'
+  'ogrenci@uni.edu.tr': '123456',
+  'guvenlik@uni.edu.tr': 'admin123',
+  'baha@uni.edu.tr': 'sincap',
+  'kaptan@uni.edu.tr': 'kaptan',
+};
